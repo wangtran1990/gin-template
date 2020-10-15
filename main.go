d13b8dbaedb79af61650a67f031d8c990a5480b0
Quang.Tran <quang@VIE-QUA-W10.localdomain>
2020-10-15 16:34:29 +0700

    optimize docker

--- main.go
+++ main.go
@@ -3,18 +3,32 @@ package main
 
 import (
 	"os"
+	"runtime"
 	configs "template/Configs"
 	routes "template/Routes"
 	services "template/Services"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	jsoniter "github.com/json-iterator/go"
 )
 
 var err error
 var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
-func main() {
+func init() {
+	// Set run CPU
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	services.Logger("", "").Infoln("Num CPU: ", runtime.NumCPU())
+
+	// Load the .env file in the current directory
+	// .env already load in-case run by docker-compose
+	err = godotenv.Load()
+	if err != nil {
+		services.Logger("", "").Warningln("Cannot get config from .env file manual - if run from docker-compose skip this warning", err)
+	} else {
+		services.Logger("", "").Infoln("Get config from .env successful")
+	}
 
 	// Set run mode
 	runmode := os.Getenv("PROD_MODE")
@@ -47,8 +61,13 @@ func main() {
 	} else {
 		services.Logger("", "").Infoln("Cache server connect successful")
 	}
+}
 
+func main() {
+	// Init routes
 	r := routes.SetupRouter()
+
+	// Start server
 	runport := os.Getenv("RUN_PORT")
 	if runport == "" {
 		runport = "2000"
